8e78ac8d940a3397397f01fb9ac014f6e1d44865
finiearalia <finiearaliashearani@mail.ugm.ac.id>
2024-11-10 18:23:14 +0700

    Acara10

--- App.tsx
+++ App.tsx
@@ -37,7 +37,7 @@ function Section({children, title}: SectionProps): React.JSX.Element {
         style={[
           styles.sectionTitle,
           {
-            color: 'green',
+            color: 'pink',
           },
         ]}>
         {title}
